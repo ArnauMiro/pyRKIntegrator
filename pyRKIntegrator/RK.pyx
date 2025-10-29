98764725ea504e6aa11feb5509b552f1a1fcaeed
Arnau Miro <arnau.miro@upc.edu>
2025-10-29 13:09:48 +0100

    Adding RK.pxd module

---
+++ pyRKIntegrator/RK.pyx
@@ -0,0 +1,6 @@
+#!/usr/bin/env cpython
+#
+# RUNGE-KUTTA Integration.
+#
+# Exporting of compiled functions.
+#cython: legacy_implicit_noexcept=True
--- pyRKIntegrator/RungeKutta.pyx
+++ pyRKIntegrator/RungeKutta.pyx
@@ -1,4 +1,4 @@
-#!/usr/bin/env cython
+#!/usr/bin/env cpython
 '''
 	RUNGE-KUTTA Integration
 
@@ -57,38 +57,7 @@ cimport numpy as np
 cimport cython
 from libc.string cimport memcpy
 
-
-# Expose functions from the RK cpp header
-cdef extern from "RK.h":
-	# Typedef for RK parameters
-	ctypedef struct RK_PARAM:
-		double h0
-		double eps
-		double epsevf
-		double minstep
-		double secfact
-		double secfact_max
-		double secfact_min
-		int    (*eventfcn)(double,double*,int,double*,int*) noexcept # Event function must return continue or stop
-		int    (*outputfcn)(double,double*,int) noexcept             # Output function must return continue or stop
-	# Typedef for RK output
-	ctypedef struct RK_OUT:
-		int    retval
-		int    n
-		double err
-		double *x
-		double *y
-		double *dy
-	# Expose the Runge-Kutta basic integrator
-	cdef RK_OUT c_odeRK "odeRK" (const char *scheme, void (*odefun)(double,double*,int,double*),
-		double xspan[2], double y0[], const int n, const RK_PARAM *rkp)
-	# Expose the Runge-Kutta-Nystrom integrator
-	cdef RK_OUT c_odeRKN "odeRKN"(const char *scheme, void (*odefun)(double,double*,int,double*),
-		double xspan[2], double y0[], double dy0[], const int n, const RK_PARAM *rkp);
-	# Expose the free rkout
-	cdef void freerkout(const RK_OUT *rko)
- 	# Expose the check tableau
-	cdef int check_tableau(const char *scheme)
+from .RK cimport RK_PARAM, RK_OUT, c_odeRK, c_odeRKN, freerkout, check_tableau
 
 
 @cython.boundscheck(False) # turn off bounds-checking for entire function
@@ -285,7 +254,7 @@ cdef class odeset:
 		return self.c_rkp.secfact_min
 	@secfact_min.setter
 	def secfact_min(self, double secfact_min):
-		self.c_rkp.secfact_min = secfact_min		
+		self.c_rkp.secfact_min = secfact_min
 
 
 def odeRK(object scheme,object fun,double[:] xspan,double[:] y0,odeset params=odeset()):
